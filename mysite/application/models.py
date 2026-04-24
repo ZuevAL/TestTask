ad22b531e706118ac9d443f0e7c79b09671ddc9a
Egor <zueve2007@gmail.com>
2026-04-24 14:46:44 +0300

    Create model Session

--- mysite/application/models.py
+++ mysite/application/models.py
@@ -8,3 +8,9 @@ class User(models.Model):
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=60)
     is_active = models.BooleanField(default=True)
+
+
+class Session(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    session = models.CharField(max_length=255)
+    expire_at = models.DateTimeField()
