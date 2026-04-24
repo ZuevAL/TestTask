4d89b13a1c1e683de15bc98bbe137adeef5f9c86
Egor <zueve2007@gmail.com>
2026-04-24 14:04:48 +0300

    Create model User

--- mysite/application/models.py
+++ mysite/application/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    patronymic = models.CharField(max_length=255, null=True)
+    email = models.EmailField(max_length=255, unique=True)
+    password = models.CharField(max_length=60)
+    is_active = models.BooleanField(default=True)
