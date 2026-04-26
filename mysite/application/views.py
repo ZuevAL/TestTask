1763b97c8e8c5a72c39036b0707eaf36594fe15a
Egor <zueve2007@gmail.com>
2026-04-26 15:17:05 +0300

    Add login view with session and cookies

--- mysite/application/views.py
+++ mysite/application/views.py
@@ -1,6 +1,9 @@
+from datetime import timedelta
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import User
+from django.utils import timezone
+from .models import User, Session
+from uuid import uuid4
 import bcrypt
 
 
@@ -27,3 +30,22 @@ def register(request):
                             password=hashed_password_decoded)
 
         return HttpResponse('Регистрация прошла успешно!')
+
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = User.objects.filter(email=email).first()
+        if user:
+            if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                random_key = str(uuid4())
+                expire_time = timezone.now() + timedelta(days=7)
+                Session.objects.update_or_create(user=user, defaults={'session': random_key, 'expire_at': expire_time})
+                response = HttpResponse('Авторизация успешно выполнена!')
+                response.set_cookie(key='sessionid', value=random_key, expires=expire_time)
+                return response
+
+            return HttpResponse('Пароль неверный')
+
+        return HttpResponse('Такого пользователя нет')
