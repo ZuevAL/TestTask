ea2b179469f4655ce6f9a67d3e1ae5b7c1e57628
Egor <zueve2007@gmail.com>
2026-04-28 16:58:05 +0300

    Add Role and Permission models

--- mysite/application/models.py
+++ mysite/application/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Permission(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    permissions = models.ManyToManyField(Permission)
+
+
 class User(models.Model):
     name = models.CharField(max_length=255)
     surname = models.CharField(max_length=255)
@@ -8,6 +17,7 @@ class User(models.Model):
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=60)
     is_active = models.BooleanField(default=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
 
 
 class Session(models.Model):
