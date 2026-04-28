f53cc30f64cfdbb08a705612cd2cb48af5595dc3
Egor <zueve2007@gmail.com>
2026-04-28 18:11:46 +0300

    Add URL routing for application views

---
+++ mysite/application/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+app_name = 'application'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('delete_account/', views.delete_account, name='delete_account'),
+    path('update_profile/', views.update_profile, name='update_profile'),
+    path('delete_article/<int:article_id>/', views.delete_article, name='delete_article'),
+]
